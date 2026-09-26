bce461ea4af70aeabb2409d4e8433fca3e31e6ff
yingsangmao <yingsangmao@users.noreply.github.com>
2026-09-26 21:55:29 +0800

    统一行尾为 CRLF
    这三个文件此前是以 CRCRLF（\r\r\n）存储的，任何编辑都会让 git 认为
    整个文件都变了，产生大量与内容无关的 diff 噪声。
    本次只把行尾统一成 CRLF，内容一字未改。

--- HARDWARE/USART/USART1.c
+++ HARDWARE/USART/USART1.c
@@ -1,71 +1,71 @@
-﻿#include "USART1.h"
-#include "sys.h"
-
-u8 RxData=0x00;
-
-/*
-*** USART1  配置串口1与JDY-31通信 ***
-
-*** 波特率：115200，8位数据位，1位停止位，无奇偶校验位 ***
-
-*** 接口：PA9->TX   PA10->RX ***
-
-*** 原作者：chenjiayou  整理与补充：yingsangmao ***
-
-
-
-*/
-void USART1_Init(uint32_t bound)
-{
-	GPIO_InitTypeDef GPIO_Initstructure;
-	NVIC_InitTypeDef NVIC_InitStruct;
-	USART_InitTypeDef USART1_InitStructure;
-
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE); 
-
-	// USART1_TX     PA9
-	GPIO_Initstructure.GPIO_Mode = GPIO_Mode_AF_PP; //复用推挽输出
-	GPIO_Initstructure.GPIO_Pin = GPIO_Pin_9;
-	GPIO_Initstructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOA, &GPIO_Initstructure);
-
-	// USART1_RX     PA10
-	GPIO_Initstructure.GPIO_Mode = GPIO_Mode_IN_FLOATING; //浮空输入
-	GPIO_Initstructure.GPIO_Pin = GPIO_Pin_10;
-	GPIO_Initstructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOA, &GPIO_Initstructure);
-
-	USART1_InitStructure.USART_BaudRate = bound;
-	USART1_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None; //无硬件数据流控制
-	USART1_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;                 //收发模式
-	USART1_InitStructure.USART_Parity = USART_Parity_No;                             //无奇偶校验位
-	USART1_InitStructure.USART_StopBits = USART_StopBits_1;                          //一个停止位
-	USART1_InitStructure.USART_WordLength = USART_WordLength_8b;                     //字长为8位数据格式
-	USART_Init(USART1, &USART1_InitStructure);
-	USART_Cmd(USART1, ENABLE); //使能USART1
-
-	NVIC_InitStruct.NVIC_IRQChannel = USART1_IRQn;
-	NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
-	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 1;
-	NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0;
-
-	NVIC_Init(&NVIC_InitStruct);
-//	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
-
-	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
-	USART_ClearFlag(USART1, USART_FLAG_TC);
-}
-
-void USART1_IRQHandler(void)
-{
-	if (USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == SET)
-	{
-			//USART_ClearFlag(USART1, USART_FLAG_RXNE);
-			RxData = USART_ReceiveData(USART1);  //接收到的数据存到 RxData
-
-//    USART_SendData(USART1, RxData);
-//    while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET) //等待数据发完
-//            ;
-	}
-}
+﻿#include "USART1.h"
+#include "sys.h"
+
+u8 RxData=0x00;
+
+/*
+*** USART1  配置串口1与JDY-31通信 ***
+
+*** 波特率：115200，8位数据位，1位停止位，无奇偶校验位 ***
+
+*** 接口：PA9->TX   PA10->RX ***
+
+*** 原作者：chenjiayou  整理与补充：yingsangmao ***
+
+
+
+*/
+void USART1_Init(uint32_t bound)
+{
+	GPIO_InitTypeDef GPIO_Initstructure;
+	NVIC_InitTypeDef NVIC_InitStruct;
+	USART_InitTypeDef USART1_InitStructure;
+
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE); 
+
+	// USART1_TX     PA9
+	GPIO_Initstructure.GPIO_Mode = GPIO_Mode_AF_PP; //复用推挽输出
+	GPIO_Initstructure.GPIO_Pin = GPIO_Pin_9;
+	GPIO_Initstructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_Initstructure);
+
+	// USART1_RX     PA10
+	GPIO_Initstructure.GPIO_Mode = GPIO_Mode_IN_FLOATING; //浮空输入
+	GPIO_Initstructure.GPIO_Pin = GPIO_Pin_10;
+	GPIO_Initstructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_Initstructure);
+
+	USART1_InitStructure.USART_BaudRate = bound;
+	USART1_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None; //无硬件数据流控制
+	USART1_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;                 //收发模式
+	USART1_InitStructure.USART_Parity = USART_Parity_No;                             //无奇偶校验位
+	USART1_InitStructure.USART_StopBits = USART_StopBits_1;                          //一个停止位
+	USART1_InitStructure.USART_WordLength = USART_WordLength_8b;                     //字长为8位数据格式
+	USART_Init(USART1, &USART1_InitStructure);
+	USART_Cmd(USART1, ENABLE); //使能USART1
+
+	NVIC_InitStruct.NVIC_IRQChannel = USART1_IRQn;
+	NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 1;
+	NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0;
+
+	NVIC_Init(&NVIC_InitStruct);
+//	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+
+	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+	USART_ClearFlag(USART1, USART_FLAG_TC);
+}
+
+void USART1_IRQHandler(void)
+{
+	if (USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == SET)
+	{
+			//USART_ClearFlag(USART1, USART_FLAG_RXNE);
+			RxData = USART_ReceiveData(USART1);  //接收到的数据存到 RxData
+
+//    USART_SendData(USART1, RxData);
+//    while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET) //等待数据发完
+//            ;
+	}
+}
--- User/mycontrol.c
+++ User/mycontrol.c
@@ -1,73 +1,73 @@
-﻿#include "stm32f10x.h"
-#include "Delay.h"
-#include "Trace.h"
-#include "MOTOR.h"
-#include "HCSR04.h"
-#include "Servo.h"
-
-void HCSR04_getData(void);
-
-
-uint16_t HCSR04_M=0;  //转到中间 超声波测到的距离
-uint16_t HCSR04_L=0;  //转到左边 超声波测到的距离
-uint16_t HCSR04_R=0;  //转到右边 超声波测到的距离
-
-
-void Avoidance(void)
-{
-	HCSR04_M = HCSR04_GetValue();    //单位厘米
-	
-	if(HCSR04_M>0&&HCSR04_M<=35)
-	{
-		Set_Car_Speed(0,0);	//停车
-		HCSR04_getData();  //读取左右距离
-		
-		if(HCSR04_L>=HCSR04_R)
-		{
-			Set_Car_Speed(-3800,-3800);
-			Delay_ms(600);
-			Set_Car_Speed(0,0);
-			Delay_ms(200);
-			Set_Car_Speed(-4300,4300);  //左转
-			Delay_ms(800);
-		}
-		else
-		{
-			Set_Car_Speed(-3800,-3800);
-			Delay_ms(600);
-			Set_Car_Speed(0,0);
-			Delay_ms(200);
-			Set_Car_Speed(4300,-4300);  //右转
-			Delay_ms(800);
-		}
-	}
-	else
-	{
-		Set_Car_Speed(3800,3800);  //直行
-	}
-	
-	
-}
-
-//超声波读取前面、左边、右边的距离
-void HCSR04_getData(void)
-{
-	Servo_SetAngle(90);
-	HCSR04_M = HCSR04_GetValue();    //单位厘米
-	
-	Servo_SetAngle(150);
-	HCSR04_L = HCSR04_GetValue();    //单位厘米
-	
-	Servo_SetAngle(30);
-	HCSR04_R = HCSR04_GetValue();    //单位厘米
-	
-	Servo_SetAngle(90);
-}
-
-
-
-
-//寻迹任务
+﻿#include "stm32f10x.h"
+#include "Delay.h"
+#include "Trace.h"
+#include "MOTOR.h"
+#include "HCSR04.h"
+#include "Servo.h"
+
+void HCSR04_getData(void);
+
+
+uint16_t HCSR04_M=0;  //转到中间 超声波测到的距离
+uint16_t HCSR04_L=0;  //转到左边 超声波测到的距离
+uint16_t HCSR04_R=0;  //转到右边 超声波测到的距离
+
+
+void Avoidance(void)
+{
+	HCSR04_M = HCSR04_GetValue();    //单位厘米
+	
+	if(HCSR04_M>0&&HCSR04_M<=35)
+	{
+		Set_Car_Speed(0,0);	//停车
+		HCSR04_getData();  //读取左右距离
+		
+		if(HCSR04_L>=HCSR04_R)
+		{
+			Set_Car_Speed(-3800,-3800);
+			Delay_ms(600);
+			Set_Car_Speed(0,0);
+			Delay_ms(200);
+			Set_Car_Speed(-4300,4300);  //左转
+			Delay_ms(800);
+		}
+		else
+		{
+			Set_Car_Speed(-3800,-3800);
+			Delay_ms(600);
+			Set_Car_Speed(0,0);
+			Delay_ms(200);
+			Set_Car_Speed(4300,-4300);  //右转
+			Delay_ms(800);
+		}
+	}
+	else
+	{
+		Set_Car_Speed(3800,3800);  //直行
+	}
+	
+	
+}
+
+//超声波读取前面、左边、右边的距离
+void HCSR04_getData(void)
+{
+	Servo_SetAngle(90);
+	HCSR04_M = HCSR04_GetValue();    //单位厘米
+	
+	Servo_SetAngle(150);
+	HCSR04_L = HCSR04_GetValue();    //单位厘米
+	
+	Servo_SetAngle(30);
+	HCSR04_R = HCSR04_GetValue();    //单位厘米
+	
+	Servo_SetAngle(90);
+}
+
+
+
+
+//寻迹任务
 void Trace_task(void)
 {
 
